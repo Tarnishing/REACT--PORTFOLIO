634fcaf1fe94aadd90273959198dbc7fdf73a3d0
Nikola Petreski <105189772+Tarnishing@users.noreply.github.com>
2022-10-21 20:54:05 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="icon1"></div>
+      <div className="icon2"></div>
+      <div className="icon3"></div>
+      <div className="icon4"></div>
+      <div className="icon5"></div>
+      <div className="icon6"></div>
+      <div className="icon7"></div>
+      <div className="icon8"></div>
+    </div>
+  );
+}
+
+export default App;
